828f027a0ad5724d7007b960ab1fc13732669d31
jaseongKim <jaseong4281@gmail.com>
2022-08-20 00:53:28 +0900

    로그인 VIew 미완성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import Login from "./pages/Login";
 import Detail from "./pages/Detail";
 import Mypage from "./pages/Mypage";
 import Posting from "./pages/Posting";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/detail/:id" element={<Detail />} />
-      <Route path="/mypage/:id" element={<Mypage />} />
-      <Route path="/posting" element={<Posting />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
+    <div className="layout">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/mypage/:id" element={<Mypage />} />
+        <Route path="/posting" element={<Posting />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Home.css";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+export default function Home() {
+  const [loginPage, setLoginPage] = React.useState(true);
+
+  const goSignUpPage = () => {
+    setLoginPage(false);
+  };
+
+  const goLoginPage = () => {
+    setLoginPage(true);
+  };
+
+  return (
+    <div className="Container">
+      <img
+        className="homeImg"
+        alt="images"
+        src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png"
+      ></img>
+      <div className="pageContainer">
+        <div className="signContainer">
+          {loginPage ? (
+            <Login></Login>
+          ) : (
+            <SignUp goLoginPage={goLoginPage}></SignUp>
+          )}
+        </div>
+        <div className="buttonContainer">
+          {loginPage ? (
+            <>
+              <div className="homeText">계정이 없으신가요</div>
+              <button className="homeButton" onClick={goSignUpPage}>
+                가입하기
+              </button>
+            </>
+          ) : (
+            <>
+              <div className="homeText">계정이 있으신가요</div>
+              <button className="homeButton" onClick={goLoginPage}>
+                로그인
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
