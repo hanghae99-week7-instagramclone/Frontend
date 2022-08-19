a552d170b064f1d1e4ed3226938fe1b484a67e23
Rita Soojin Woo <linawoo@naver.com>
2022-08-20 00:52:58 +0900

    마이페이지 틀 작업 초안

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./MypageStyle.css";
 
 const Mypage = () => {
-  return <div>Mypage</div>;
+  return (
+    <div className="whole-mypage">
+      <div className="wrap-info">
+        <div className="image-area">
+          <div className="my-image">프로필 이미지</div>
+        </div>
+        <div className=""></div>
+        <div>newjeans_official</div>
+        <button>팔로우</button>
+        <div>게시물</div>
+        <div>게시물수</div>
+        <div>팔로워</div>
+        <div>팔로워수</div>
+        <div>팔로우</div>
+        <div>팔로우수</div>
+        <div>사용자이름</div>
+      </div>
+      <div className="mypage-navbar"></div>
+      <div>게시물</div>
+      <div>릴스</div>
+      <div>동영상</div>
+      <div>태그됨</div>
+      <div>사진박스</div>
+      <div>사진1</div>
+    </div>
+  );
 };
 
 export default Mypage;
