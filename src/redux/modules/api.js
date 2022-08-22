5ba0f408dbb2aee39283b98066656fe58f38849d
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2022-08-22 11:39:43 +0900

    Create api.js

---
+++ src/redux/modules/api.js
@@ -0,0 +1,9 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: process.env.REACT_APP_URL,
+// });
+// const token = localStorage.getItem("token");
+// api.defaults.headers.common["authorization"] = token ? `${token}` : null;
+
+// export default api;
