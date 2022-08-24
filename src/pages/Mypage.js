915f83a0980f8d6465959b225097afbdfdae5ab2
jaseongKim <jaseong4281@gmail.com>
2022-08-25 00:18:05 +0900

    리덕스 모듈 변경

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -14,15 +14,18 @@ import {
 import FollowToggle from "../redux/modules/mypageSlice";
 import postListSlice from "../redux/modules/postListSlice";
 import { useNavigate } from "react-router-dom";
+import { asyncGetOneMemberProfile } from "../redux/modules/memberSlice";
 
 const Mypage = () => {
+  const memberId = localStorage.getItem("id");
   const dispatch = useDispatch();
   //const { isLoading, error, mypage, stateOfFollow } = useSelector(
   const navigate = useNavigate();
-  const mypage = useSelector((state) => state.mypage.mypage);
+  const member = useSelector((state) => state.member.member);
   const postImageList = useSelector((state) => state.mypage.postImageList);
-
-  console.log("Checking mypage", mypage);
+  const [image, setImage] = useState(
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  );
 
   // const { mypage } = useSelector((state) => state.mypage);
   // (원래 이렇게 했다가 데이터 불러오는거 실패함. 경로도 틀렸고, const mypage의 중괄호도 빼야함)
@@ -54,13 +57,12 @@ const Mypage = () => {
   // };
 
   //TODO
-  const tempMemberId = 23;
 
   useEffect(() => {
     //memberId값을 넣어야함
-    dispatch(getMypageThunk(tempMemberId));
-    dispatch(getPostImageListThunk(tempMemberId));
-  }, [dispatch]);
+    dispatch(asyncGetOneMemberProfile(localStorage.getItem("id")));
+    dispatch(getPostImageListThunk(memberId));
+  }, [dispatch, memberId]);
 
   // 프로필 수정 버튼에서
   // {isMe?
@@ -98,12 +100,16 @@ const Mypage = () => {
           <div className="wrap-info">
             <div className="image-area">
               <div className="my-image">
-                <img src="../../55.png"></img>
+                <img
+                  src={member?.profileUrl ? member.profileUrl : image}
+                  alt=""
+                  style={{ width: "170px", height: "170px" }}
+                ></img>
               </div>
             </div>
             <div className="right-info">
               <div className="info-line-1">
-                <div className="user-nickname">{mypage?.nickname}</div>
+                <div className="user-nickname">{member?.nickname}</div>
                 <button
                   onClick={() => {
                     navigate("/ReviseMypage");
@@ -129,21 +135,21 @@ const Mypage = () => {
               <div className="info-line-2">
                 <div className="post-area">
                   <div className="mypage-post">게시물</div>
-                  <div className="post-number">{mypage?.postCount}</div>
+                  <div className="post-number">{member?.postCount}</div>
                 </div>
                 <div className="follower-area">
                   <div className="mypage-follower">팔로워</div>
-                  <div className="follower-number">{mypage?.follower}</div>
+                  <div className="follower-number">{member?.follower}</div>
                 </div>
                 <div className="follow-area">
                   <div className="mypage-follow">팔로우</div>
-                  <div className="follow-number">{mypage?.follow}</div>
+                  <div className="follow-number">{member?.follow}</div>
                 </div>
               </div>
               <div className="info-line-3">
-                <div className="mypage-username">{mypage?.username}</div>
-                <div className="mypage-bio">{mypage?.bio}</div>
-                <div className="mypage-url">{mypage?.websiteUrl}</div>
+                <div className="mypage-username">{member?.username}</div>
+                <div className="mypage-bio">{member?.bio}</div>
+                <div className="mypage-url">{member?.websiteUrl}</div>
               </div>
             </div>
           </div>
